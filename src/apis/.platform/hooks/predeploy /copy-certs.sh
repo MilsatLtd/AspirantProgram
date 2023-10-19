8a6243dd1db723998737e43d119f47e41e3418ef
chidera okoye <141339293+dera-milsat@users.noreply.github.com>
2023-10-19 03:50:36 -0700

    Update copy-certs.sh

--- src/apis/.platform/hooks/predeploy /copy-certs.sh
+++ src/apis/.platform/hooks/predeploy /copy-certs.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Download the certificates from S3
-sudo aws s3 cp s3://ssl-tls-certificates-bucket/Fluid-monitoring-certs/fullchain.pem /etc/pki/tls/certs/fullchain.pem
-sudo aws s3 cp s3://ssl-tls-certificates-bucket/Fluid-monitoring-certs/privkey.pem /etc/pki/tls/private/privkey.pem
+sudo aws s3 cp s3://ssl-tls-certificates-bucket/map-api-certs/fullchain.pem /etc/pki/tls/certs/fullchain.pem
+sudo aws s3 cp s3://ssl-tls-certificates-bucket/map-api-certs/privkey.pem /etc/pki/tls/private/privkey.pem
