4f19fc785a283e22c0803aaf39cce10d0040631f
chidera okoye <141339293+dera-milsat@users.noreply.github.com>
2023-10-22 14:38:57 -0700

    Delete src/apis/.platform directory

--- src/apis/.platform/hooks/predeploy/copy-certs.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-# Download the certificates from S3
-sudo aws s3 cp s3://ssl-tls-certificates-bucket/map-api-certs/fullchain.pem /etc/pki/tls/map-api/fullchain.pem
-sudo aws s3 cp s3://ssl-tls-certificates-bucket/map-api-certs/privkey.pem /etc/pki/tls/map-api/privkey.pem
