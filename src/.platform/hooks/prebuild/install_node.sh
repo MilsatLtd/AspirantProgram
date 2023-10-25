13b80360eab67d4558a3f7bc13235ba3d2f1950a
chidera okoye <141339293+dera-milsat@users.noreply.github.com>
2023-10-25 04:24:47 -0700

    Update install_node.sh

--- src/.platform/hooks/prebuild/install_node.sh
+++ src/.platform/hooks/prebuild/install_node.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
-sudo apt-get install -y nodejs npm
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+# Export NVM environment variable
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads NVM
+
+# Install Node.js version 16.16.0 using NVM
+nvm install 16
+
+# Ensure npm is available
+nvm use 16.16.0
