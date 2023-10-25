5eae0c7fa6a4dbf1a03228474d94d920a2381aac
chidera okoye <141339293+dera-milsat@users.noreply.github.com>
2023-10-25 05:51:50 -0700

    Update install_node.sh

--- src/.platform/hooks/prebuild/install_node.sh
+++ src/.platform/hooks/prebuild/install_node.sh
@@ -7,6 +7,6 @@ curl -fsSL https://nodejs.org/dist/v16.13.0/node-v16.13.0-linux-x64.tar.xz -o no
 sudo tar -Jxf node-v16.13.0-linux-x64.tar.xz -C /usr/local
 
 # Create a symbolic link to make node and npm commands available system-wide
-sudo ln -s /usr/local/node-v16.13.0-linux-x64/bin/node /usr/local/bin/node
-sudo ln -s /usr/local/node-v16.13.0-linux-x64/bin/npm /usr/local/bin/npm
-sudo ln -s /usr/local/node-v16.13.0-linux-x64/bin/npx /usr/local/bin/npx
+# sudo ln -s /usr/local/node-v16.13.0-linux-x64/bin/node /usr/local/bin/node
+# sudo ln -s /usr/local/node-v16.13.0-linux-x64/bin/npm /usr/local/bin/npm
+# sudo ln -s /usr/local/node-v16.13.0-linux-x64/bin/npx /usr/local/bin/npx
