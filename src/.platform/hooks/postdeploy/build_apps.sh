ddda4192fb55462fd09506afe52182b5fcb690aa
chidera okoye <141339293+dera-milsat@users.noreply.github.com>
2023-10-25 04:30:23 -0700

    Update build_apps.sh

--- src/.platform/hooks/postdeploy/build_apps.sh
+++ src/.platform/hooks/postdeploy/build_apps.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
 cd /var/app/current/admin && npm install && npm run build
 cd /var/app/current/web && npm install && npm run build
+cd /var/app/currentapis && pip install -r requirements.txt && python manage.py collectstatic --noinput
+        
