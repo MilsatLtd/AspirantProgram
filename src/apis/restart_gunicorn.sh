341ca6dc03dce8dd297df03a80ab80f4b42af84d
chidera okoye <141339293+dera-milsat@users.noreply.github.com>
2023-11-09 14:19:01 -0800

    Create restart_gunicorn.sh

---
+++ src/apis/restart_gunicorn.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Gunicorn settings
+APP_NAME="map.wsgi:application"
+BIND_ADDRESS="0.0.0.0:8000"
+
+echo "Restarting Gunicorn processes for $APP_NAME"
+
+# Gracefully terminate the old Gunicorn processes
+pkill -f "gunicorn.*$APP_NAME"
+
+# Wait for a moment to ensure that the resources are freed
+sleep 5
+
+# Start a new Gunicorn daemon
+gunicorn $APP_NAME --bind $BIND_ADDRESS &
+
+echo "Gunicorn restarted for $APP_NAME"
+
+#Set the DJANGO_SETTINGS_MODULE Environment Variable
+export DJANGO_SETTINGS_MODULE=map.settings
+
+#Run Migrations
+python3 manage.py migrate
