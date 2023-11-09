0cc771a1923dbce8cf65f3fc329712d4c293f7f9
chidera okoye <141339293+dera-milsat@users.noreply.github.com>
2023-11-09 14:15:40 -0800

    Delete src/.platform directory

--- src/.platform/hooks/postdeploy/build_apps.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cd /var/app/current/admin && npm install && npm run build
-cd /var/app/current/web && npm install && npm run build
-cd /var/app/current/apis && pip install -r requirements.txt
-        
