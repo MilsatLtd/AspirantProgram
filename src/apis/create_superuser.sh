3853ec5baa824495c69201215bd954aa5c7e4b6f
chidera okoye <141339293+dera-milsat@users.noreply.github.com>
2023-11-21 09:53:12 -0800

    Update create_superuser.sh

--- src/apis/create_superuser.sh
+++ src/apis/create_superuser.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-python3 manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@milsat.tech').exists() or User.objects.create_superuser('admin@milsat.com', first_name='map', last_name='admin', gender=0, country='Nigeria', phone_number='07039496535', bio='', password='Password123?_')"
+python3 manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@milsat.tech').exists() or User.objects.create_superuser('admin@milsat.tech', first_name='map', last_name='admin', gender=0, country='Nigeria', phone_number='07039496535', bio='', password='Password123?_')"
