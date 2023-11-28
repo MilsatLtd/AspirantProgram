759230daaa1e5c529e4bd5888d88abce026b1b95
Matthew Oke <mathew.oke@milsat.tech>
2023-11-28 04:25:26 +0100

    send any email by admin

--- src/apis/api/tasks.py
+++ src/apis/api/tasks.py
@@ -1,4 +1,7 @@
 from celery import shared_task
+import html2text
+from django.core.mail import send_mail
+from django.conf import settings
 from .models import Cohort
 from .common.enums import *
 import logging
@@ -26,5 +29,14 @@ def cohort_live_to_end(cohort_id):
         for cohort in cohorts:
             cohort.status = COHORT_STATUS.ENDED.value
             cohort.save()
+    except Exception as e:
+        logger.exception(e)
+
+@shared_task()
+def send_html_email_task(subject, recipient, message):
+    try:
+        sender = settings.EMAIL_HOST_USER
+        plaintext = html2text.HTML2Text().handle(message)
+        send_mail(subject, plaintext, sender, recipient, html_message=message, fail_silently=False)
     except Exception as e:
         logger.exception(e)
