e233ed3f2d3f73141e6ef1b21f687eeb261dc9c7
Matthew Oke <mathew.oke@milsat.tech>
2024-02-27 12:48:23 +0100

    cleanup

--- src/apis/api/migrations/0015_delete_prod_blockers.py
+++ src/apis/api/migrations/0015_delete_prod_blockers.py
@@ -7,6 +7,19 @@ def delete_prod_blockers(apps, schema_editor):
     for blocker in Blocker.objects.all():
         blocker.delete()
 
+    # remove the migration record from django_migrations table
+    custom_sql = """
+    DELETE FROM django_migrations WHERE app='api' AND name='0015_delete_prod_blockers';
+    """
+
+    # Execute the SQL query
+    with schema_editor.connection.cursor() as cursor:
+        cursor.execute(custom_sql)
+
+    # delete this file itself
+    import os
+    # os.remove(__file__)
+
 class Migration(migrations.Migration):
 
     dependencies = [
