3eaada04fdba530af3f8abe97bce795f60fcf410
Matthew Oke <mathew.oke@milsat.tech>
2024-02-27 18:13:15 +0100

    cleanup slack log from dev

--- src/apis/map/settings_dev.py
+++ src/apis/map/settings_dev.py
@@ -261,12 +261,6 @@ LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
     'handlers': {
-        'slack': {
-            'level': 'ERROR',
-            'class': 'api.logging.SlackLogHandler',
-            'logging_url': 'https://hooks.slack.com/services/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',
-            'stack_trace': True
-        },
         'console': {
             'level': 'ERROR',
             'class': 'logging.StreamHandler',
